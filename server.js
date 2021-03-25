21b78a404b86418a0e8ade13cd7b41e2d4b73ee7
Nicolas Farolfi <nicolasfarolfi@MacBook-Air-de-Nicolas.local>
2021-03-25 10:57:11 +0400

    deploy heroku

---
+++ server.js
@@ -0,0 +1,45 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+const cors = require('cors')
+
+const app = express();
+
+
+// CORS
+// const whitelist = ['https://sea-locker-backend.herokuapp.com']; // list of allow domain
+//
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (!origin) {
+//             return callback(null, true);
+//         }
+//
+//         if (whitelist.indexOf(origin) === -1) {
+//             var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     }
+// }
+//
+// // end
+// app.use(cors(corsOptions));
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/demo-tempo'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/demo-tempo/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
